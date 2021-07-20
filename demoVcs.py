6dd863162672c48087010a8b227f7a8a28bf8f6e
juliechoo-iad <46205845+juliechoo-iad@users.noreply.github.com>
2021-07-20 22:39:10 +0800

    Added multiply function

--- demoVcs.py
+++ demoVcs.py
@@ -1,9 +1,13 @@
 class Calculator:
     def add(num1, num2):
-        print("SUm is ",(num1+num2))
+        print("Sum is ",(num1+num2))
 
     def minus(num1, num2):
         print("Difference is ",(num1 - num2))
+    def multiply(num1, num2):
+        print("Product is ",(num1 * num2))
+
 
 Calculator.add(50,40)
 Calculator.minus(50,40)
+Calculator.multiply(50,40)
