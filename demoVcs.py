8cac76bd8df572ce1af01e9242936f4f3e43a4c9
juliechoo-iad <46205845+juliechoo-iad@users.noreply.github.com>
2021-07-20 22:35:52 +0800

    Create class Calculator

---
+++ demoVcs.py
@@ -0,0 +1,9 @@
+class Calculator:
+    def add(num1, num2):
+        print("SUm is ",(num1+num2))
+
+    def minus(num1, num2):
+        print("Difference is ",(num1 - num2))
+
+Calculator.add(50,40)
+Calculator.minus(50,40)
